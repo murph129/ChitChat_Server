5cfd42380e8eb1c68836785a0f9f2423bfe7401d
Nathan Stanley <nstanley@purdue.edu>
2016-12-02 14:26:29 -0500

    Add and Update both work in CmdSocketListener

--- CommServer/CommServer/src/CmdSocketListener.java
+++ CommServer/CommServer/src/CmdSocketListener.java
@@ -51,6 +51,7 @@ public class CmdSocketListener implements Runnable {
                     out.println("Invalid Command");
                 }
                 s.close();
+                socket.close(); 
                 socket = null;
                 s = null;
             } catch (IOException ex) {
@@ -61,14 +62,32 @@ public class CmdSocketListener implements Runnable {
     
     public void addUser(String[] command) throws IOException
     {
+        
+        // Write to profile.csv
         File f = new File(System.getProperty("java.class.path"));
         File dir = f.getAbsoluteFile().getParentFile();
         String path = dir.toString();
         String profilePath = path+ "\\profile.csv";
+        File profile = new File(profilePath);
+        if(profile.createNewFile()) {
+            System.out.println("Created profile.csv");
+        }
+        System.out.println("Writing to "+profilePath);
         FileWriter writer = new FileWriter(profilePath,true);
         BufferedWriter bw = new BufferedWriter(writer);
-        bw.write(command[1]+","+command[2]+","+command[3]+","+command[4]+","+command[5]+"\n");
-        writer.close();
+        bw.write(command[1]+","+command[2]+","+command[3]+","+command[4]+","+command[5]+","+command[6]+"\n");
+        bw.close();
+        
+        // Write to cred.csv
+        String credPath = path+"\\cred.csv";
+        File cred = new File(credPath);
+        if(cred.createNewFile()) {
+            System.out.println("Created cred.csv");
+        }
+        bw = new BufferedWriter(new FileWriter(credPath, true));
+        System.out.println("Writing to "+credPath);
+        bw.write(command[1]+","+command[2]+"\n");
+        bw.close();
     }
     
     public void updateUser(String[] command) throws IOException
@@ -78,29 +97,35 @@ public class CmdSocketListener implements Runnable {
         File dir = f.getAbsoluteFile().getParentFile();
         String path = dir.toString();
         String profilePath = path + "\\profile.csv";
+        String credPath = path + "\\cred.csv";
         
+        // Check the existence of the csvs
+        if(!new File(profilePath).exists()) {
+            System.out.println(profilePath + " does not exist.  Aborting");
+            return;
+        } else if(!new File(credPath).exists()) {
+            System.out.println(credPath + " does not exist. Aborting");
+            return;
+        }
+        
+        
+        /**************************
+         * UPDATE PROFILE.CSV
+         *************************/
 
         // Create a reader to read in the csv file
         BufferedReader reader = new BufferedReader(new FileReader(profilePath));
         // Read the profile CSV into memory
         String line;
         ArrayList data = new ArrayList();
-        /*************************
-         * FOR DEBUGGING ONLY
-         ************************/
+        System.out.println("Examining PROFILE.CSV");
         System.out.println("Looking for " + command[1]);
         while((line = reader.readLine()) != null) {
-            // Check to see if edits need to be made
-
-            /********************************************
-             * FOR DEBUGGING ONLY
-             *******************************************/
             System.out.println("Looking at " + line.split(",")[0]);
-
             if(line.split(",")[0].equals(command[1])) { // Username matches: use new information
-                data.add(command[1]+","+command[2]+","+command[3]+","+command[4]+","+command[5]+"\n");
+                data.add(command[1]+","+command[2]+","+command[3]+","+command[4]+","+command[5]+","+command[6]+"\n");
             } else { // Username does not match: use existing information
-                data.add(line);
+                data.add(line+"\n");
             }
         } // End While (read file)
 
@@ -108,12 +133,49 @@ public class CmdSocketListener implements Runnable {
         reader.close();
 
         // Write new data to file
-        FileWriter writer = new FileWriter(profilePath, false); // False, to overwrite
+        BufferedWriter writer = new BufferedWriter(new FileWriter(profilePath, false)); // False, to overwrite
+        System.out.println("Writing to "+profilePath);
         for(int i = 0; i < data.size(); i++) {
             writer.write(data.get(i).toString()); // ToString needed; data is an arraylist of objects, writer.write only accepts strings & chars
         }
         
-        // Explicitly close the writer, because why not
+        // Close writer
+        writer.close();
+        
+        
+        
+        
+        /**************************
+         * UPDATE CRED.CSV
+         *************************/
+        
+        // Create a reader to read in the csv file
+        reader = new BufferedReader(new FileReader(credPath));
+        data = new ArrayList(); // Reset data
+        
+        // Read the profile CSV into memory
+        System.out.println("Examining CRED.CSV");
+        System.out.println("Looking for "+command[1]);
+        while((line = reader.readLine()) != null) {
+            System.out.println("Looking at " + line.split(",")[0]);
+            if(line.split(",")[0].equals(command[1])) { // If there is a match of usernames
+                data.add(command[1] + "," + command[2] + "\n");
+            } else { // There is no match: use the existing data
+                data.add(line + "\n");
+            }
+        } // End While (read file)
+        
+        // Close the reader to open up the write lock
+        reader.close();
+        
+        // Create a new writer to write the data to the cred.csv
+        writer = new BufferedWriter(new FileWriter(credPath, false)); // False, to overwrite
+        System.out.println("Writing to " + credPath);
+        for(int i = 0; i < data.size(); i++) {
+            writer.write(data.get(i).toString()); // ToString needed; data is an arraylist of objects, writer.write only accepts strings & chars
+        }
+        
+        // Close writer
         writer.close();
         
     }
